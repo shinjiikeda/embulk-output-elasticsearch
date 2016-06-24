23491d8061c5b5cef030905dca46876510b03459
shinjiikeda <gm.ikeda@gmail.com>
2016-06-24 17:53:39 +0900

    fix change id bug.

--- src/main/java/org/embulk/output/elasticsearch/ElasticsearchOutputPlugin.java
+++ src/main/java/org/embulk/output/elasticsearch/ElasticsearchOutputPlugin.java
@@ -1,18 +1,9 @@
 package org.embulk.output.elasticsearch;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-import java.util.concurrent.TimeUnit;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-
+import com.google.common.base.Optional;
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableList;
+import com.google.inject.Inject;
 import org.elasticsearch.action.bulk.BulkItemResponse;
 import org.elasticsearch.action.bulk.BulkProcessor;
 import org.elasticsearch.action.bulk.BulkRequest;
@@ -20,47 +11,40 @@ import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.Requests;
-import org.elasticsearch.client.transport.NoNodeAvailableException;
 import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.cluster.metadata.AliasMetaData;
-import org.elasticsearch.cluster.metadata.AliasOrIndex;
-import org.elasticsearch.common.collect.ImmutableOpenMap;
+import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.common.unit.ByteSizeUnit;
+import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.common.unit.ByteSizeValue;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
-import org.elasticsearch.action.admin.cluster.state.ClusterStateRequest;
-import org.elasticsearch.action.admin.indices.alias.get.GetAliasesRequest;
-import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
-import org.elasticsearch.index.IndexNotFoundException;
-import org.elasticsearch.indices.InvalidAliasNameException;
-
+import org.elasticsearch.node.Node;
+import org.elasticsearch.node.NodeBuilder;
+import org.embulk.config.TaskReport;
 import org.embulk.config.Config;
 import org.embulk.config.ConfigDefault;
 import org.embulk.config.ConfigDiff;
-import org.embulk.config.ConfigException;
 import org.embulk.config.ConfigSource;
 import org.embulk.config.Task;
-import org.embulk.config.TaskReport;
 import org.embulk.config.TaskSource;
-import org.embulk.config.UserDataException;
 import org.embulk.spi.Column;
-import org.embulk.spi.ColumnVisitor;
 import org.embulk.spi.Exec;
 import org.embulk.spi.OutputPlugin;
 import org.embulk.spi.Page;
 import org.embulk.spi.PageReader;
 import org.embulk.spi.Schema;
+import org.embulk.spi.ColumnVisitor;
 import org.embulk.spi.TransactionalPageOutput;
-import org.embulk.spi.time.Timestamp;
 import org.embulk.spi.type.Types;
 import org.slf4j.Logger;
 
-import com.google.common.base.Optional;
-import com.google.common.base.Throwables;
-import com.google.inject.Inject;
-import com.carrotsearch.hppc.cursors.ObjectObjectCursor;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.google.common.base.Preconditions.checkState;
 
 public class ElasticsearchOutputPlugin
         implements OutputPlugin
@@ -79,10 +63,6 @@ public class ElasticsearchOutputPlugin
     public interface PluginTask
             extends Task
     {
-        @Config("mode")
-        @ConfigDefault("\"insert\"")
-        public Mode getMode();
-
         @Config("nodes")
         public List<NodeAddressTask> getNodes();
 
@@ -92,12 +72,6 @@ public class ElasticsearchOutputPlugin
 
         @Config("index")
         public String getIndex();
-        public void setIndex(String indexName);
-
-        @Config("alias")
-        @ConfigDefault("null")
-        public Optional<String> getAlias();
-        public void setAlias(Optional<String> aliasName);
 
         @Config("index_type")
         public String getType();
@@ -135,26 +109,10 @@ public class ElasticsearchOutputPlugin
 
         // confirm that a client can be initialized
         try (Client client = createClient(task)) {
-            log.info(String.format("Executing plugin with '%s' mode.", task.getMode()));
-            if (task.getMode().equals(Mode.REPLACE)) {
-                task.setAlias(Optional.of(task.getIndex()));
-                task.setIndex(generateNewIndexName(task.getIndex()));
-                if (isExistsIndex(task.getAlias().orNull(), client) && !isAlias(task.getAlias().orNull(), client)) {
-                    throw new ConfigException(String.format("Invalid alias name [%s], an index exists with the same name as the alias", task.getAlias().orNull()));
-                }
-            }
-            log.info(String.format("Inserting data into index[%s]", task.getIndex()));
-            control.run(task.dump());
+        }
 
-            if (task.getMode().equals(Mode.REPLACE)) {
-                try {
-                    reAssignAlias(task.getAlias().orNull(), task.getIndex(), client);
-                } catch (IndexNotFoundException | InvalidAliasNameException e) {
-                    throw new ConfigException(e);
-                } catch (NoNodeAvailableException e) {
-                    throw new ConnectionException(e);
-                }
-            }
+        try {
+            control.run(task.dump());
         } catch (Exception e) {
             throw Throwables.propagate(e);
         }
@@ -176,22 +134,19 @@ public class ElasticsearchOutputPlugin
     public void cleanup(TaskSource taskSource,
                         Schema schema, int processorCount,
                         List<TaskReport> successTaskReports)
-    {}
+    { }
 
     private Client createClient(final PluginTask task)
     {
         //  @see http://www.elasticsearch.org/guide/en/elasticsearch/client/java-api/current/client.html
-        Settings settings = Settings.settingsBuilder()
+        Settings settings = ImmutableSettings.settingsBuilder()
+                .classLoader(Settings.class.getClassLoader())
                 .put("cluster.name", task.getClusterName())
                 .build();
-        TransportClient client = TransportClient.builder().settings(settings).build();
+        TransportClient client = new TransportClient(settings);
         List<NodeAddressTask> nodes = task.getNodes();
         for (NodeAddressTask node : nodes) {
-            try {
-                client.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(node.getHost()), node.getPort()));
-            } catch (UnknownHostException | NoNodeAvailableException e) {
-                throw new ConnectionException(e);
-            }
+            client.addTransportAddress(new InetSocketTransportAddress(node.getHost(), node.getPort()));
         }
         return client;
     }
@@ -229,12 +184,7 @@ public class ElasticsearchOutputPlugin
             @Override
             public void afterBulk(long executionId, BulkRequest request, Throwable failure)
             {
-                if (failure.getClass() == NoNodeAvailableException.class) {
-                    log.error("Got the error during bulk processing", failure);
-                    throw new ConnectionException(failure);
-                } else {
-                    log.warn("Got the error during bulk processing", failure);
-                }
+                log.warn("Got the error during bulk processing", failure);
             }
         }).setBulkActions(task.getBulkActions())
           .setBulkSize(new ByteSizeValue(task.getBulkSize()))
@@ -247,6 +197,7 @@ public class ElasticsearchOutputPlugin
                                         int processorIndex)
     {
         final PluginTask task = taskSource.loadTask(PluginTask.class);
+
         Client client = createClient(task);
         BulkProcessor bulkProcessor = newBulkProcessor(task, client);
         ElasticsearchPageOutput pageOutput = new ElasticsearchPageOutput(task, client, bulkProcessor);
@@ -401,16 +352,12 @@ public class ElasticsearchOutputPlugin
                     contextBuilder.endObject();
                     bulkProcessor.add(newIndexRequest(getIdValue(idColumn)).source(contextBuilder));
 
-                } catch (ConnectionException | IOException e) {
+                } catch (IOException e) {
                     Throwables.propagate(e); //  TODO error handling
                 }
             }
         }
 
-        /**
-         * @param inputColumn
-         * @return
-         */
         private String getIdValue(Column inputColumn) {
             if (inputColumn == null) return null;
             if (pageReader.isNull(inputColumn)) return null;
@@ -423,8 +370,6 @@ public class ElasticsearchOutputPlugin
                 idValue = pageReader.getDouble(inputColumn) + "";
             } else if (Types.LONG.equals(inputColumn.getType())) {
                 idValue = pageReader.getLong(inputColumn) + "";
-            } else if (Types.JSON.equals(inputColumn.getType())) {
-                idValue = pageReader.getJson(inputColumn).toJson();
             } else if (Types.TIMESTAMP.equals(inputColumn.getType())) {
                 idValue = pageReader.getTimestamp(inputColumn).toString();
             } else {
@@ -432,7 +377,7 @@ public class ElasticsearchOutputPlugin
             }
             return idValue;
         }
-
+ 
         private IndexRequest newIndexRequest(String idValue)
         {
             return Requests.indexRequest(index).type(type).id(idValue);
@@ -483,104 +428,4 @@ public class ElasticsearchOutputPlugin
         }
 
     }
-
-    public enum Mode
-    {
-        INSERT,
-        REPLACE;
-
-        @JsonValue
-        @Override
-        public String toString()
-        {
-            return name().toLowerCase(Locale.ENGLISH);
-        }
-
-        @JsonCreator
-        public static Mode fromString(String value)
-        {
-            switch (value) {
-                case "insert":
-                    return INSERT;
-                case "replace":
-                    return REPLACE;
-                default:
-                    throw new ConfigException(String.format("Unknown mode '%s'. Supported modes are insert, truncate_insert, replace", value));
-            }
-        }
-    }
-
-    private void reAssignAlias(String aliasName, String newIndexName, Client client)
-            throws IndexNotFoundException, InvalidAliasNameException
-    {
-        if (!isExistsAlias(aliasName, client)) {
-            client.admin().indices().prepareAliases()
-                    .addAlias(newIndexName, aliasName)
-                    .execute().actionGet();
-            log.info(String.format("Assigned alias[%s] to index[%s]", aliasName, newIndexName));
-        } else {
-            List<String> oldIndices = getIndexByAlias(aliasName, client);
-            client.admin().indices().prepareAliases()
-                    .removeAlias(oldIndices.toArray(new String[oldIndices.size()]), aliasName)
-                    .addAlias(newIndexName, aliasName)
-                    .execute().actionGet();
-            log.info(String.format("Reassigned alias[%s] from index%s to index[%s]", aliasName, oldIndices, newIndexName));
-            for (String index : oldIndices) {
-                deleteIndex(index, client);
-            }
-        }
-    }
-
-    private void deleteIndex(String indexName, Client client)
-    {
-        client.admin().indices().delete(new DeleteIndexRequest(indexName)).actionGet();
-        log.info(String.format("Deleted Index [%s]", indexName));
-    }
-
-    private List<String> getIndexByAlias(String aliasName, Client client)
-    {
-        ImmutableOpenMap<String, List<AliasMetaData>> map = client.admin().indices().getAliases(new GetAliasesRequest(aliasName))
-                .actionGet().getAliases();
-        List<String> indices = new ArrayList<>();
-        for (ObjectObjectCursor<String, List<AliasMetaData>> c : map) {
-            indices.add(c.key);
-        }
-
-        return indices;
-    }
-
-    private boolean isExistsAlias(String aliasName, Client client)
-    {
-        return client.admin().cluster().state(new ClusterStateRequest()).actionGet().getState().getMetaData().hasAlias(aliasName);
-    }
-
-    private boolean isExistsIndex(String indexName, Client client)
-    {
-        return client.admin().cluster().state(new ClusterStateRequest()).actionGet().getState().getMetaData().hasIndex(indexName);
-    }
-
-    private boolean isAlias(String aliasName, Client client)
-    {
-        AliasOrIndex aliasOrIndex = client.admin().cluster().state(new ClusterStateRequest()).actionGet().getState().getMetaData().getAliasAndIndexLookup().get(aliasName);
-        return aliasOrIndex != null && aliasOrIndex.isAlias();
-    }
-
-    public String generateNewIndexName(String indexName)
-    {
-        Timestamp time = Exec.getTransactionTime();
-        return indexName + new SimpleDateFormat("_yyyyMMdd-HHmmss").format(time.toEpochMilli());
-    }
-
-    public class ConnectionException extends RuntimeException implements UserDataException
-    {
-        protected ConnectionException()
-        {
-        }
-
-        public ConnectionException(Throwable cause)
-        {
-            super(cause);
-        }
-    }
-
 }
